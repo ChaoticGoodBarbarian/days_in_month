4026dade4ce4f730a895f28f30c023294910c98b
Chaotic Good Barbarian <tazdingodan@gmail.com>
2025-11-23 13:37:53 +0200

    done, not elegant but working

--- days_in_month.cpp
+++ days_in_month.cpp
@@ -2,13 +2,41 @@
 
 int main() {
 	int year;
+	int month;
 
-	std::cin >> year;
+	std::cin >> month >> year;
+
+	if ((month < 1) || (month > 12) ||
+		(year < 1) || (year > 9999)) {
+		std::cout << "ENTER VALID DATA\n";
+	}
 
 	if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)) {
-		std::cout << "YES\n";
+		if (month == 2) {
+			std::cout << 29;
+		}
+		else if (month == 4 ||
+			month == 6 ||
+			month == 9 ||
+			month == 11) {
+			std::cout << 30;
+		}
+		else {
+			std::cout << 31;
+		}
 	}
 	else {
-		std::cout << "NO\n";
+		if (month == 2) {
+			std::cout << 28;
+		}
+		else if (month == 4 ||
+			month == 6 ||
+			month == 9 ||
+			month == 11) {
+			std::cout << 30;
+		}
+		else {
+			std::cout << 31;
+		}
 	}
 }
